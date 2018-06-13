21663252509b710d65385fa143038f2e40b264e2
Wayne Liang <wlia631@aucklanduni.ac.nz>
2018-06-13 21:14:37 +1200

    Performance improved by requesting 200 skills each time

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ const readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
 const results = [];
-const DELAY = 500;
-
-const checkSkill = (skillId, keyword) => {
-  axios.get(`https://api.guildwars2.com/v2/skills?ids=${skillId}`).then(res => {
-    const skillData = res.data[0];
-    if (skillData.name.toLowerCase().includes(keyword.toLowerCase())) {
-      results.push(skillData);
-    }
-
-    console.log(`Searching (${skillId})`);
-  });
-};
 
 rl.question('Search for: ', answer => {
   axios.get('https://api.guildwars2.com/v2/skills').then(res => {
     const skillIds = res.data;
-    skillIds.forEach((skillId, index) =>
-      setTimeout(checkSkill, index * DELAY, skillId, answer),
-    );
 
-    setTimeout(
-      finalResults => {
-        finalResults.forEach(result => {
-          console.log(result);
+    for (let i = 0; i < skillIds.length; i += 200) {
+      const skillIdChunk = skillIds.slice(i, i + 200);
+      let requestURL = 'https://api.guildwars2.com/v2/skills?ids=';
+      skillIdChunk.forEach((skillId, index) => {
+        requestURL += skillId;
+        requestURL += ',';
+      });
+      requestURL = requestURL.slice(0, -1);
+
+      axios
+        .get(requestURL)
+        .then(res => {
+          const skills = res.data;
+
+          skills.forEach(skill => {
+            if (skill.name.toLowerCase().includes(answer.toLowerCase())) {
+              console.log(`${skill.id}: ${skill.name}`);
+            }
+          });
+        })
+        .catch(err => {
+          console.log('ERROR', err);
         });
-      },
-      (skillIds.length + 1) * DELAY,
-      results,
-    );
+    }
   });
+
+  rl.close();
 });
--- test.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://api.guildwars2.com/v2/skills?ids=41218').then(res => {
-  const skillData = res.data;
-  console.log(skillData.name);
-  console.log(skillData);
-});
